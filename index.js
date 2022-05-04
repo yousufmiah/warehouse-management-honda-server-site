8c7508bd0994fb2b55307601e2aa43c20d8ae2a5
yousufmiah <yousufweb99@gmail.com>
2022-05-04 06:49:38 +0600

    edited

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
-const port = process.env.POR || 5001;
+const port = process.env.PORT || 5001;
 const app = express();
 
 //middleware
@@ -11,6 +11,8 @@ app.use(express.json());
 
 //connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yyoht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -50,6 +52,8 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+console.log("all route ok");
 
 //root api
 app.get("/", (req, res) => {
