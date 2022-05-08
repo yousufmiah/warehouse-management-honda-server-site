700b4c18929dde66d1742bbe37afa73300749e7c
yousufmiah <yousufweb99@gmail.com>
2022-05-08 19:14:49 +0600

    edited

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 require("dotenv").config();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 4200;
 const app = express();
 
 //middleware
